12a6bdadb1b361a2460563b5fad70abf502ff4b7
bryankroesbeek <bryank.b96@gmail.com>
2018-11-25 01:42:20 +0100

    Add routes for CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   get '/', to: 'home#index'
+  get '/api/:table/columns', to: 'api#get_columns'
+  get '/api/database/tables', to: 'api#get_tables'
+
+  get '/api/:table/all', to: 'api#get_model'
+  post '/api/:table/create', to: 'api#create_model'
+  put '/api/:table/update', to: 'api#update_model'
+  delete '/api/:table/:id', to: 'api#delete_model'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
