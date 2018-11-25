452c0a85ed617bd6f7a9ac1428d53e4bf6c2ad2f
bryankroesbeek <bryank.b96@gmail.com>
2018-11-25 01:42:51 +0100

    Add method to convert object names to lowercase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    def downcase_keys(model_data)
+        return unless model_data.present?
+
+        data = [model_data].flatten
+
+        colnames = data.first.class.column_names
+        data = data.map do |row|
+            obj = {}
+            colnames.each do |col|
+                obj[col.underscore] = row[col]
+            end
+            obj
+        end
+
+        return data if data.length > 1
+        return data.first
+    end
 end
