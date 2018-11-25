52406b85e17f3f573370460675c85228187a75ad
bryankroesbeek <bryank.b96@gmail.com>
2018-11-25 21:43:24 +0100

    Add route for frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get '/', to: 'home#index'
   get '/api/:table/columns', to: 'api#get_columns'
   get '/api/database/tables', to: 'api#get_tables'
-
+  
   get '/api/:table/all', to: 'api#get_model'
   post '/api/:table/create', to: 'api#create_model'
   put '/api/:table/:id/update', to: 'api#update_model'
   delete '/api/:table/:id', to: 'api#delete_model'
-
+  
+  get '/', to: 'home#index'
+  get '/*path', to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
