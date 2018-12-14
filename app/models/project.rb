0c9b8a8757dc7ef122e72de21b93fd0d5a88d078
bryankroesbeek <bryank.b96@gmail.com>
2018-12-14 18:58:44 +0100

    Add methods to project to convert from binary string to boolean and back

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
 class Project < ApplicationRecord
     self.table_name='Project'
+
+    before_update :boolean_to_binary_string
+    after_find :binary_string_to_boolean
+
+    def boolean_to_binary_string
+        self.Visible = self.Visible ? "\x01" : "\x00"
+    end
+
+    def binary_string_to_boolean 
+        self.Visible = self.Visible == "\x01"
+    end
 end
