3451ef10b13090aa41fc92850ba102783ff30c2d
bryankroesbeek <bryank.b96@gmail.com>
2018-12-26 15:37:22 +0100

    Add extra conversion

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,12 +3,19 @@ class Project < ApplicationRecord
 
     before_update :boolean_to_binary_string
     after_find :binary_string_to_boolean
+    before_create :initialize_project
+    after_create :binary_string_to_boolean
 
     def boolean_to_binary_string
         self.Visible = self.Visible ? "\x01" : "\x00"
     end
 
     def binary_string_to_boolean 
-        self.Visible = self.Visible == "\x01"
+        self.Visible = self.Visible.b == "\x01".b
+    end
+
+    def initialize_project
+        self.Visible = "\x00"
+        self.Position = 1 + Project.count
     end
 end
