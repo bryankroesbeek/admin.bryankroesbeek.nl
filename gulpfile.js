448c9debf2677a1f99e4b7a3b661ae77e93d6b1b
bryankroesbeek <bryank.b96@gmail.com>
2019-09-25 21:02:54 +0200

    Update package-lock and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,23 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gsass = require('gulp-sass');
 var concat = require('gulp-concat');
 var cleanCss = require('gulp-clean-css');
 
 var cssPath = 'Client/stylesheets/**/*.css';
 var sassPath = 'Client/stylesheets/**/*.scss';
 
-gulp.task('css', () => {
+function sass() {
     return gulp.src(cssPath)
+        .pipe(gsass())
         .pipe(cleanCss())
         .pipe(concat('style.min.css'))
         .pipe(gulp.dest('app/assets/stylesheets/'))
-});
+};
 
-gulp.task('sass', () => {
-    return gulp.src(sassPath)
-        .pipe(sass())
-        .pipe(cleanCss())
-        .pipe(concat('style.min.css'))
-        .pipe(gulp.dest('app/assets/stylesheets/'))
-});
+function watch() {
+    gulp.watch([cssPath, sassPath], sass);
+};
 
-gulp.task('watch', () => {
-    gulp.watch([cssPath, sassPath], ['css', 'sass']);
-});
-
-gulp.task('default', ['css', 'sass']);
+exports.sass = sass;
+exports.watch = watch;
+exports.default = sass;
